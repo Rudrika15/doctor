b3635a612c58018b7e95398f9c4d2e87941cf2cc
unknown <manishaparmar2796@gmail.com>
2023-05-29 16:07:07 +0530

    Correct speliing mistack of hospital type and remove same name menu in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,21 +109,12 @@
                         <a class="text-white waves-effect" href="{{route('hospital.index')}}"><i class="text-white menu-icon bi bi-hospital-fill"></i><span>Hospital</span></a>
                     </li>
                     <li>
-                        <a class="text-white waves-effect" href="{{route('hospitaltype.index')}}"><i class="text-white menu-icon bi bi-building-add"></i><span>Hoapital Type</span></a>
+                        <a class="text-white waves-effect" href="{{route('hospitaltype.index')}}"><i class="text-white menu-icon bi bi-building-add"></i><span>Hospital Type</span></a>
                     </li>
-                        <li>
-                            <a class="text-white waves-effect" href="{{route('city.index')}}"><i class="text-white menu-icon bi bi-buildings"></i><span>City</span></a>
-                        </li>
-                        <li>
-                            <a class="text-white waves-effect" href="{{route('hospital.index')}}"><i class="text-white menu-icon bi bi-hospital-fill"></i><span>Hospital</span></a>
-                        </li>
-                        <li>
-                            <a class="text-white waves-effect" href="{{route('hospitaltype.index')}}"><i class="text-white menu-icon bi bi-building-add"></i><span>Hospital Type</span></a>
-                        </li>
                         
-                        <li>
-                            <a class="text-white waves-effect" href="{{route('specialist.index')}}"><i class="text-white menu-icon bi bi-file-person-fill"></i><span>Specialist</span></a>
-                        </li>
+                    <li>
+                        <a class="text-white waves-effect" href="{{route('specialist.index')}}"><i class="text-white menu-icon bi bi-file-person-fill"></i><span>Specialist</span></a>
+                    </li>
                     @endrole
                     
                     @role("Hospital")
